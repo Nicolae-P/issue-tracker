d649b1104fea7dd08b4e6d41a8b669019247d807
Nicolae Pogila <nikupogila@gmail.com>
2024-02-06 10:47:22 +0200

    Add loading skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -6,7 +6,6 @@ import {
   TableBody,
   TableCell,
 } from "@radix-ui/themes";
-import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./IssueActions";
